be2948f6dafac8abfb8f10f83b75c42a9a9d58bd
Abdulaziz Ikromov <abdulazizikromov17@gmail.com>
2023-02-16 19:46:26 +0500

    footer end!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,12 @@
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
 const mode = document.querySelector(".mode");
 const modeIcon = document.querySelector("#mode-icon");
+const loginBtn = document.querySelector("#btn-login");
+const login = document.querySelector(".login");
+const form_exit = document.querySelector(".form_exit");
+const iconEye = document.querySelector(".icon-eye");
+const password = document.querySelector("#password");
+const email = document.querySelector("#email");
+const form = document.querySelector(".form");
 
 if (
   localStorage.theme === "dark" ||
@@ -20,7 +26,7 @@ if (
 
 //  Whenever the user explicitly chooses light mode
 
-//  Whenever the user explicitly chooses dark mode
+//  ! CHoose mode theme
 mode.addEventListener("click", (e) => {
   if (localStorage.theme === "dark") {
     document.documentElement.classList.remove("dark");
@@ -35,20 +41,187 @@ mode.addEventListener("click", (e) => {
   }
 });
 
-const modalWindow = document.querySelector(".modal-window"),
-  signUpBtn = document.querySelector(".register-btn"),
-  modalContent = document.querySelector(".modal-content"),
-  closeBtn = document.querySelector(".close-btn");
-
-signUpBtn.addEventListener("click", (e) => {
-  modalWindow.classList.remove("hidden");
-});
-closeBtn.addEventListener("click", (e) => {
-  modalWindow.classList.add("hidden");
+// !----login modal
+loginBtn.addEventListener("click", (e) => {
+  login.classList.remove("hidden");
+  login.classList.add("flex");
 });
 
-modalWindow.addEventListener("click", (e) => {
-  if (e.target.classList.contains("modal-window")) {
-    modalContent.classList.toggle("shaker");
+login.addEventListener("click", (e) => {
+  if (e.target.classList.contains("login")) {
+    login.classList.add("hidden");
+    login.classList.remove("flex");
+    password.value = "";
+    email.value = "";
   }
+});
+
+form_exit.addEventListener("click", (e) => {
+  login.classList.add("hidden");
+  login.classList.remove("flex");
+  password.value = "";
+  email.value = "";
+});
+
+iconEye.addEventListener("click", (e) => {
+  if (e.target.classList.contains("bxs-show")) {
+    iconEye.classList.remove("bxs-show");
+    iconEye.classList.add("bxs-hide");
+    password.type = "text";
+  } else {
+    iconEye.classList.remove("bxs-hide");
+    iconEye.classList.add("bxs-show");
+    password.type = "password";
+  }
+});
+
+
+const swiper = new Swiper(".swiper", {
+  spaceBetween: 30,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    },
+  },
+});
+
+//------- comments ---------//
+const comments = [
+  {
+    id: 1,
+    avatar: "./images/Ava.svg",
+    name: "Savannah Nguyen",
+    email: "savannahnguyen@gmail.com",
+    comment: `Sagittis sed tortor, elementum vitae varius sodales
+    consectetur. Vitae arcu ut morbi bibendum ullamcorper
+    feugiat. Venenatis hendrerit cursus egestas sit ornare nunc
+    vulputate aliquam sapien. Pulvinar nunc consectetur nibh
+    consectetur mollis mauris est mauris amet. Sed hac vehicula
+    id et dui morbi quam pulvinar condimentum. Senectus sed
+    varius interdum nunc, arcu vulputate massa.`,
+    rating: "./images/Frame 5292.svg",
+  },
+  {
+    id: 2,
+    avatar: "./images/Ava (1).svg",
+    name: "Ronald Richards",
+    email: "ronaldrichards@gmail.com",
+    comment: `Placerat pulvinar in amet sem id gravida nisi leo. A dignissim est pellentesque mi quisque vel mi ut. Porttitor blandit auctor odio a ornare felis tortor bibendum molestie. `,
+    rating: "./images/Frame 5292 (1).svg",
+  },
+  {
+    id: 3,
+    avatar: "./images/Ava (2).svg",
+    name: "Kristin Watson",
+    email: "kristinwatson@gmail.com",
+    comment: `Id adipiscing vulputate arcu pretium dignissim. Felis vulputate tellus velit consectetur. Congue convallis aliquam eget ullamcorper quam pellentesque cursus. Dictumst mauris ac, quis orci morbi ultrices faucibus volutpat mauris. Bibendum sed ut lectus facilisi amet orci. Interdum massa orci urna id ac cras arcu.`,
+    rating: "./images/Frame 5292.svg",
+  },
+  {
+    id: 3,
+    avatar: "./images/Ava (2).svg",
+    name: "Kristin Watson",
+    email: "kristinwatson@gmail.com",
+    comment: `Id adipiscing vulputate arcu pretium dignissim. Felis vulputate tellus velit consectetur. Congue convallis aliquam eget ullamcorper quam pellentesque cursus. Dictumst mauris ac, quis orci morbi ultrices faucibus volutpat mauris. Bibendum sed ut lectus facilisi amet orci. Interdum massa orci urna id ac cras arcu.`,
+    rating: "./images/Frame 5292.svg",
+  },
+  {
+    id: 3,
+    avatar: "./images/Ava (2).svg",
+    name: "Kristin Watson",
+    email: "kristinwatson@gmail.com",
+    comment: `Id adipiscing vulputate arcu pretium dignissim. Felis vulputate tellus velit consectetur. Congue convallis aliquam eget ullamcorper quam pellentesque cursus. Dictumst mauris ac, quis orci morbi ultrices faucibus volutpat mauris. Bibendum sed ut lectus facilisi amet orci. Interdum massa orci urna id ac cras arcu.`,
+    rating: "./images/Frame 5292.svg",
+  },
+];
+const comment_list = document.querySelector(".comment_list");
+const scrollbar = document.querySelector(".scrollbar");
+comments.forEach((el) => {
+  const element = createElement(
+    "li",
+    "comment__item w-[420px] h-[333px] rounded-2xl bg-white dark:bg-[#1C1C1C] p-6",
+    `
+  <div class="flex justify-between items-center">
+                    <div class="flex">
+                      <img src="${el.avatar}" alt="ava" />
+                      <div class="ml-4">
+                        <h1 class="font-semibold text-4 left-5">
+                        ${el.name}
+                        </h1>
+                        <h2
+                          class="font-normal text-[14px] left-[17px] text-[#555555] dark:text-white"
+                        >
+                        ${el.email}
+                        </h2>
+                      </div>
+                    </div>
+                    <img src="./images/Group 5.svg" alt="google" class="" />
+                  </div>
+                  <p
+                    class="w-[372px] h-[160px] font-normal text-[14px] left-[20px] mt-[14px]"
+                  >
+                  ${el.comment}
+                  </p>
+                  <div class="mt-[25px] h-[1.5px] bg-[#E5E7EA]"></div>
+                  <div class="flex items-center mt-[14px]">
+                    <h4
+                      class="font-medium text-[14px] inline-block left-[17px]"
+                    >
+                      Star rating:
+                    </h4>
+                    <img
+                      src="${el.rating}"
+                      alt="stars"
+                      class="ml-[17px]"
+                    />
+                  </div>
+  `
+  );
+  comment_list.append(element);
+  const scrbr = createElement(
+    "div",
+    `scrbr-item w-[${546 / comments.length}px] h-1 bg-white opacity-[0.4]`,
+    ""
+  );
+  scrollbar.append(scrbr);
+});
+document.querySelector(".scrbr-item").classList.add("active-scroll");
+
+let count = 0;
+const slider__right = document.querySelector(".slider__right");
+const slider__left = document.querySelector(".slider__left");
+
+slider__right.addEventListener("click", (e) => {
+  document.querySelector(".active-scroll").classList.remove("active-scroll");
+  if (count === comments.length - 1) {
+    count = -1;
+  }
+  comment_list.style.transform = `translateX(-${++count * 420}px)`;
+  const scr = document.querySelectorAll(".scrbr-item");
+  scr[count].classList.add("active-scroll");
+});
+slider__left.addEventListener("click", (e) => {
+  document.querySelector(".active-scroll").classList.remove("active-scroll");
+
+  if (count === 0) {
+    count = comments.length;
+  }
+  comment_list.style.transform = `translateX(-${--count * 420}px)`;
+  const scr = document.querySelectorAll(".scrbr-item");
+  scr[count].classList.add("active-scroll");
 });
